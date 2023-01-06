83852f6a37e74d24c90c525c55fabfdfd896a4f8
kipngetich <kipngetich@finplusgroup.com>
2023-01-06 08:14:33 +0300

    add code to calculate BOM total

--- erpnext/manufacturing/doctype/bom/bom.js
+++ erpnext/manufacturing/doctype/bom/bom.js
@@ -652,12 +652,18 @@ frappe.ui.form.on("BOM Item", {
 
 
 frappe.ui.form.on("BOM Item", "qty", function(frm, cdt, cdn) {
+	// calculate total qty
+	calculate_total()
+
 	var d = locals[cdt][cdn];
 	d.stock_qty = d.qty * d.conversion_factor;
 	refresh_field("stock_qty", d.name, d.parentfield);
 });
 
 frappe.ui.form.on("BOM Item", "item_code", function(frm, cdt, cdn) {
+	// calculate total qty
+	calculate_total()
+
 	var d = locals[cdt][cdn];
 	frappe.db.get_value('Item', {name: d.item_code}, 'allow_alternative_item', (r) => {
 		d.allow_alternative_item = r.allow_alternative_item
@@ -690,6 +696,22 @@ frappe.ui.form.on("BOM Item", "items_remove", function(frm) {
 	erpnext.bom.calculate_rm_cost(frm.doc);
 	erpnext.bom.calculate_total(frm.doc);
 });
+
+const calculate_total = () => {
+	let items = cur_frm.doc.items
+
+	let total_qty = 0
+	cur_frm.doc.items.forEach((x) => {
+		if(x.qty){
+			total_qty += parseFloat(x.qty)
+		}else{
+			total_qty += 1
+		}
+	})
+
+	cur_frm.set_value("total_qty",total_qty)
+	refresh_field("total_qty");
+}
 
 frappe.tour['BOM'] = [
 	{
