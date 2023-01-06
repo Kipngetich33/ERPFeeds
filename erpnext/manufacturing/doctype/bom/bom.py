3562cdc543eb9a5bb58c51a719f9dd396f2ad359
kipngetich <kipngetich@finplusgroup.com>
2023-01-06 08:28:08 +0300

    add bom invoice

--- erpnext/manufacturing/doctype/bom/bom.py
+++ erpnext/manufacturing/doctype/bom/bom.py
@@ -170,6 +170,15 @@ class BOM(WebsiteGenerator):
 
 		return index
 
+	def before_save(self):
+		total_qty = 0
+		# caculate the total
+		for item in self.items:
+			total_qty += item.qty
+		# set total amount
+		if self.total_qty != total_qty:
+			self.total_qty = total_qty
+
 	def validate(self):
 		self.route = frappe.scrub(self.name).replace("_", "-")
 
