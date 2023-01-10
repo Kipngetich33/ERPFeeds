b7fc4b7b4582a6b6aba5442edee24d2eb513d84b
kipngetich <kipngetich@finplusgroup.com>
2023-01-10 16:02:39 +0300

    Add Mixing Charge to queries filters

--- erpnext/controllers/queries_custom.py
+++ erpnext/controllers/queries_custom.py
@@ -267,7 +267,7 @@ def item_query(doctype, txt, searchfield, start, page_len, filters, as_dict=Fals
 		where tabItem.docstatus < 2
 			and tabItem.disabled=0
 			and tabItem.has_variants=0
-			and (tabItem.item_group = "Materials" or tabItem.item_group = "Additives")
+			and (tabItem.item_group = "Materials" or tabItem.item_group = "Additives" or tabItem.item_group = "MIXING CHARGE" )
 			and (tabItem.end_of_life > %(today)s or ifnull(tabItem.end_of_life, '0000-00-00')='0000-00-00')
 			and ({scond} or tabItem.item_code IN (select parent from `tabItem Barcode` where barcode LIKE %(txt)s)
 				{description_cond})
