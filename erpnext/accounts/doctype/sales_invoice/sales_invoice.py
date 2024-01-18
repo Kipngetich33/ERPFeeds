8636a9190bc82aad151c6a706502ab5ee1e37c81
Kipngetich33 <Khalifngeno@gmail.com>
2024-01-18 23:07:52 +0300

    Update sales_invoice.py
    bring back update stock

--- erpnext/accounts/doctype/sales_invoice/sales_invoice.py
+++ erpnext/accounts/doctype/sales_invoice/sales_invoice.py
@@ -291,8 +291,8 @@ class SalesInvoice(SellingController):
 				Go to <b>Payments Section</b> and enter amounts to allocate from Advance payments')
 
 		# check if invoice is updating stock
-		if self.update_stock:
-			self.update_stock = 0
+		#if self.update_stock:
+		#	self.update_stock = 0
 
 	def on_submit(self):
 		self.validate_pos_paid_amount()
